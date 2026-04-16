f491b116e9e521d6391f6d33a6e82f2e791d1b16
Javier Morilla <JavichuMB@gmail.com>
2026-04-16 16:47:26 +0200

    fix: final programmatic immersive fix for android 16 and ide type fixes

--- android/app/src/main/java/com/javiermorilla/skullking/MainActivity.java
+++ android/app/src/main/java/com/javiermorilla/skullking/MainActivity.java
@@ -1,5 +1,6 @@
 package com.javiermorilla.skullking;
 
+import android.os.Build;
 import android.os.Bundle;
 import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
@@ -9,5 +10,10 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            getWindow().setStatusBarContrastEnforced(false);
+            getWindow().setNavigationBarContrastEnforced(false);
+        }
     }
 }
